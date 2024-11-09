4b039343f7f12fa083150902a0e5e894ac5b8056
MOHAMMED BAASHN <ir.mnmn.maob@gmail.com>
2024-11-09 00:15:13 +0000

    111

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@akf8mppm9)-_u+(e+udtsa_9mmi1v-di392n57y2lqpniqwr+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-mhmdbaashn-mbblog-tkzccce6xxg.ws-eu116.gitpod.io',
+ALLOWED_HOSTS = ['8000-mhmdbaashn-mbblog-1fx33e8krrn.ws-eu116.gitpod.io',
                  '.herokuapp.com']
 
 
